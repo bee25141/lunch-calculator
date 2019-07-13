2c3a315898dfe5c9b955d315f871899b2be0a69a
bee25141 <bee25141@gmail.com>
2019-07-12 19:23:16 -0500

    bar graph correctly displays on analysis page

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,10 +1,5 @@
 //On document ready
 $(document).ready(function () {
-
-    //setting the current date and time in browser placeholder
-    let dateTimePlaceholder = new Date();
-    $("#today").innerHTML = dateTimePlaceholder;
-
     //Inputting meal stats
     $(".submit").on("click", function () {
         event.preventDefault();
@@ -25,8 +20,6 @@ $(document).ready(function () {
             let description = $(".descriptionInput").val().trim().toLowerCase();
             let cost = $(".costInput").val().trim();
             let weight = $(".weightInput").val().trim();
-            let dateRaw = $(".dateTimeInput").val().trim();
-            let dateTime = moment(dateRaw).format('MMMM Do YYYY, h:mm a');
             let analysis = lunchAnalysis(cost, weight);
 
             let newData = {
@@ -35,13 +28,11 @@ $(document).ready(function () {
                 description: description,
                 weight: weight,
                 cost: cost,
-                dateTime: dateTime,
                 analysis: analysis
             };
 
             //Populating and displaying stats in the modal
             $("#restaurantSummary").append(restaurant);
-            $("#dateTimeSummary").append(dateTime);
             $("#costSummary").append(`$ ${cost}`);
             $("#analysisSummary").append(`Your meal at ${restaurant} cost $ ${analysis} per pound`);
 
@@ -56,23 +47,10 @@ $(document).ready(function () {
             $(".descriptionInput").val("");
             $(".weightInput").val("");
             $(".costInput").val("");
-            $(".dateTimeInput").val("");
         });
     });
-    getGraph();
+    getGraph(barGraphDisplay);
 });
-
-//Function for getting all restaurant averages for graph
-let getGraph = function(){
-
-    $.get("/api/graph", function(data){
-        // console.log(data)
-        for(i=0; i<data.length; i++){
-            console.log(data[i]);
-        }
-    });
-
-};
 
 //Function for determining average cost per pound
 let lunchAnalysis = function (cost, weight) {
@@ -81,20 +59,177 @@ let lunchAnalysis = function (cost, weight) {
     return analysis;
 };
 
-//Function for getting the average cost per pound at each restaurant
-// let getAnalysis = function (restaurant, data) {
-//     var restaurant = restaurant;
-//     let dataSnap = data;
-//     dataSnap = (Object.values(dataSnap));
-//     for (i = 0; i < dataSnap.length; i++) {
-//         let dataSumArray = [(dataSnap[i].analysis)];
-//         var dataSum = dataSumArray.reduce(function (a, b) {
-//             return a + b;
-//         }, 0) / (dataSumArray.length);
-//     }
-//     var restaurantAnalysis = {
-//         restaurant: restaurant,
-//         value: dataSum
-//     };
-//     analysisArray.push(restaurantAnalysis);
-// };
+//Function for getting all restaurant averages for graph
+let analysisArray = [];
+let getGraph = function(callback){
+
+    $.get("/api/graph", function(data){
+        for(i=0; i<data.length; i++){
+            let graphData = {
+                restaurant: data[i].restaurant,
+                value: data[i].average
+            }
+            analysisArray.push(graphData);
+        };
+    }).then(callback);
+
+};
+
+//Function for displaying D3 bargraph
+function barGraphDisplay() {
+
+    const svg = d3.select('svg');
+    const svgContainer = d3.select('#container');
+    for (i=0; i<analysisArray.length; i++){
+        var dataSet = analysisArray[i].value;
+    };
+
+    const margin = 50;
+    const width = 1000 - 2 * margin;
+    const height = 600 - 2 * margin;
+
+    const chart = svg.append('g')
+        .attr('transform', `translate(${margin}, ${margin})`);
+
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(analysisArray.map((s) => s.restaurant))
+        .padding(0.75)
+
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, 40])
+
+    // vertical grid lines
+    // const makeXLines = () => d3.axisBottom()
+    //   .scale(xScale)
+
+    const makeYLines = () => d3.axisLeft()
+        .scale(yScale)
+
+    chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
+
+    chart.append('g')
+        .call(d3.axisLeft(yScale));
+
+    // vertical grid lines
+    // chart.append('g')
+    //   .attr('class', 'grid')
+    //   .attr('transform', `translate(0, ${heigcoht})`)
+    //   .call(makeXLines()
+    //     .tickSize(-height, 0, 0)
+    //     .tickFormat('')
+    //   )
+
+    chart.append('g')
+        .attr('class', 'grid')
+        .call(makeYLines()
+            .tickSize(-width, 0, 0)
+            .tickFormat('')
+        )
+    // console.log(analysisArray.length)
+    const barGroups = chart.selectAll()
+        .data(analysisArray)
+        .enter()
+        .append('g')
+
+    barGroups
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', (g) => xScale(g.restaurant))
+        .attr('y', (g) => yScale(g.value))
+        .attr('height', (g) => height - yScale(g.value))
+        .attr('width', xScale.bandwidth())
+    // .on('mouseenter', function (actual, i) {
+    //     d3.selectAll('.value')
+    //         .attr('opacity', 0)
+
+    // d3.select(this)
+    //     .transition()
+    //     .duration(300)
+    //     .attr('opacity', 0.6)
+    //     .attr('x', (a) => xScale(a.language) - 5)
+    //     .attr('width', xScale.bandwidth() + 10)
+
+    //     const y = yScale(actual.value)
+
+    //     line = chart.append('line')
+    //         .attr('id', 'limit')
+    //         .attr('x1', 0)
+    //         .attr('y1', y)
+    //         .attr('x2', width)
+    //         .attr('y2', y)
+
+    //     barGroups.append('text')
+    //         .attr('class', 'divergence')
+    //         .attr('x', (a) => xScale(a.language) + xScale.bandwidth() / 2)
+    //         .attr('y', (a) => yScale(a.value) + 30)
+    //         .attr('fill', 'white')
+    //         .attr('text-anchor', 'middle')
+    //         .text((a, idx) => {
+    //             const divergence = (a.value - actual.value).toFixed(1)
+
+    //             let text = ''
+    //             if (divergence > 0) text += '+'
+    //             text += `${divergence}%`
+
+    //             return idx !== i ? text : '';
+    //         })
+
+    // })
+    // .on('mouseleave', function () {
+    //     d3.selectAll('.value')
+    //         .attr('opacity', 1)
+
+    //     d3.select(this)
+    //         .transition()
+    //         .duration(300)
+    //         .attr('opacity', 1)
+    //         .attr('x', (a) => xScale(a.language))
+    //         .attr('width', xScale.bandwidth())
+
+    //     chart.selectAll('#limit').remove()
+    //     chart.selectAll('.divergence').remove()
+    // })
+
+    barGroups
+        .append('text')
+        .attr('class', 'value')
+        .attr('x', (a) => xScale(a.restaurant) + xScale.bandwidth() / 2)
+        .attr('y', (a) => yScale(a.value) + 30)
+        .attr('text-anchor', 'middle')
+        .text((a) => `$${a.value}`)
+
+    svg
+        .append('text')
+        .attr('class', 'label')
+        .attr('x', -(height / 2) - margin)
+        .attr('y', margin / 2.4)
+        .attr('transform', 'rotate(-90)')
+        .attr('text-anchor', 'middle')
+        .text('$ / Pound')
+
+    svg.append('text')
+        .attr('class', 'label')
+        .attr('x', width / 2 + margin)
+        .attr('y', height + margin * 1.7)
+        .attr('text-anchor', 'middle')
+        .text('Restaurants')
+
+    svg.append('text')
+        .attr('class', 'title')
+        .attr('x', width / 2 + margin)
+        .attr('y', 40)
+        .attr('text-anchor', 'middle')
+        .text('Average Cost of Meal Per Pound')
+
+    // svg.append('text')
+    //     .attr('class', 'source')
+    //     .attr('x', width - margin / 2)
+    //     .attr('y', height + margin * 1.7)
+    //     .attr('text-anchor', 'start')
+    //     .text('Source: Stack Overflow, 2018')
+
+};
