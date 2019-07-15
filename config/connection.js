a8083814d3cc634f281a71d3eef5bdeee7873658
bee25141 <bee25141@gmail.com>
2019-07-15 16:39:13 -0500

    updated db connection to aws rds instance

--- config/connection.js
+++ config/connection.js
@@ -2,20 +2,20 @@
 const mysql = require("mysql");
 
 // Establishing connection to the database
+// let connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 8889,
+//     user: "root",
+//     password: "root",
+//     database: "lunch_box_chi"
+// });
 let connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
+    host: "test123.cql2tzwph3cy.us-east-1.rds.amazonaws.com",
+    port: 3306,
+    user: "test123",
+    password: process.env.AWS_RDSDB,
     database: "lunch_box_chi"
 });
-// let connection = mysql.createConnection({
-//     host: "lunchboxchi.cql2tzwph3cy.us-east-1.rds.amazonaws.com",
-//     port: 3306,
-//     user: "bee25141",
-//     password: "password",
-//     database: "lunchboxchi"
-// });
 
 // Make connection.
 connection.connect(function (err) {
