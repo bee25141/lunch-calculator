2822f7c194e61e3d74ec979a2b342d1854d2c80b
bee25141 <bee25141@gmail.com>
2019-08-19 21:24:41 -0500

    changed routing to public index.html

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname, '../client', 'build'));
+  res.sendFile(path.join(__dirname, '../client', 'public'));
 });
 
 // Start the API server
