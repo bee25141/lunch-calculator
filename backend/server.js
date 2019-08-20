380c4e59ebbd6789a1bc87e1381bf11463660d2d
bee25141 <bee25141@gmail.com>
2019-08-19 20:49:09 -0500

    testing server.js route

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 app.get('*', (req,res) =>{
-  res.sendFile(path.join(path.join(__dirname, '../client/build/index.html')));
+  res.sendFile(path.join(__dirname, '../client', 'build'));
 });
 
 // Start the API server
