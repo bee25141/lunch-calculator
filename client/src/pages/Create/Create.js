b053c54a97118b63613af2dc0495840ed355f356
bee25141 <bee25141@gmail.com>
2019-09-17 13:04:49 -0500

    able to update state hooks on value change

--- client/src/pages/Create/Create.js
+++ client/src/pages/Create/Create.js
@@ -4,14 +4,63 @@ import {Card, CardBody, CardTitle} from "shards-react";
 
 export default function Create() {
 
-        const [user, setUser] = useState({
-            userName: "",
-            password: ""
+        const [user, setValues] = useState({
+            username:  "",
+            email:  "",
+            password:  "",
+            passwordConfirm:  "",
         });
+
+        const updateUser = event => {
+
+            setValues({
+                [event.target.name]: event.target.value 
+            }, callback())
+        }
+
+        const createUser = event => {
+            event.preventDefault();
+
+            let userObject = {
+                username: user.username,
+                email: user.email,
+                password: user.password
+            }
+            
+            console.log(userObject);
+        }
+
+        const callback = () => {
+            console.log(user.username)
+            console.log(user.email)
+            console.log(user.password)
+            console.log(user.passwordConfirm)
+        }
 
         return (
             <Form>
-                
+                    <FormGroup>
+                        <label htmlFor="#username">Username</label>
+                        <FormInput id="#username" value={user.username} name="username"
+                        onChange={updateUser} />
+                    </FormGroup>
+                    <FormGroup>
+                        <label htmlFor="#email">Email</label>
+                        <FormInput id="#email" value={user.email} name="email"
+                        onChange={updateUser} />
+                    </FormGroup>
+                    <FormGroup>
+                        <label htmlFor="#password">Password</label>
+                        <FormInput type="password" id="#password" value={user.password} name="password"
+                        onChange={updateUser} />
+                    </FormGroup>
+                    <FormGroup>
+                        <label htmlFor="#passwordConfirm">Confirm Password</label>
+                    <FormInput type="password" id="passwordConfirm" value={user.passwordConfirm} name="passwordConfirm"
+                        onChange={updateUser} />
+                    </FormGroup>
+
+                    <Button theme="primary" onClick={(event) => createUser()} >Submit</Button>
             </Form>
         )
 }
