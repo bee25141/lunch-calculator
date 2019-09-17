e829c57b42d1adab0fa69c58c0c5cf484fcc67b3
bee25141 <bee25141@gmail.com>
2019-09-17 13:11:01 -0500

    updated styling to add bootstrap colums for centering

--- client/src/pages/Create/Create.js
+++ client/src/pages/Create/Create.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { Form, FormInput, FormGroup, Button } from "shards-react";
+import {Container, Row, Col} from "../../components/Grid/Grid"
 import {Card, CardBody, CardTitle} from "shards-react";
 
 export default function Create() {
@@ -38,29 +39,37 @@ export default function Create() {
         }
 
         return (
-            <Form>
-                    <FormGroup>
-                        <label htmlFor="#username">Username</label>
-                        <FormInput id="#username" value={user.username} name="username"
-                        onChange={updateUser} />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="#email">Email</label>
-                        <FormInput id="#email" value={user.email} name="email"
-                        onChange={updateUser} />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="#password">Password</label>
-                        <FormInput type="password" id="#password" value={user.password} name="password"
-                        onChange={updateUser} />
-                    </FormGroup>
-                    <FormGroup>
-                        <label htmlFor="#passwordConfirm">Confirm Password</label>
-                    <FormInput type="password" id="passwordConfirm" value={user.passwordConfirm} name="passwordConfirm"
-                        onChange={updateUser} />
-                    </FormGroup>
+            <Container>
+                    <Row>
 
-                    <Button theme="primary" onClick={(event) => createUser()} >Submit</Button>
-            </Form>
+                            <Col size="md-2"></Col>
+                            <Col size="md-8">
+                                    <Form>
+                                            <FormGroup>
+                                                <label htmlFor="#username">Username</label>
+                                                <FormInput id="#username" value={user.username} name="username"
+                                                onChange={updateUser} />
+                                            </FormGroup>
+                                            <FormGroup>
+                                                <label htmlFor="#email">Email</label>
+                                                <FormInput id="#email" value={user.email} name="email"
+                                                onChange={updateUser} />
+                                            </FormGroup>
+                                            <FormGroup>
+                                                <label htmlFor="#password">Password</label>
+                                                <FormInput type="password" id="#password" value={user.password} name="password"
+                                                onChange={updateUser} />
+                                            </FormGroup>
+                                            <FormGroup>
+                                                <label htmlFor="#passwordConfirm">Confirm Password</label>
+                                            <FormInput type="password" id="passwordConfirm" value={user.passwordConfirm} name="passwordConfirm"
+                                                onChange={updateUser} />
+                                            </FormGroup>
+
+                                            <Button theme="primary" onClick={(event) => createUser()} >Submit</Button>
+                                    </Form>
+                            </Col>
+                    </Row>
+            </Container>
         )
 }
