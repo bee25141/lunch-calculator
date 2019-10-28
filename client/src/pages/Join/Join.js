bcd5b656a42084c8767aeaf019213eff12972da3
bee25141 <bee25141@gmail.com>
2019-10-28 14:41:06 -0500

    setting state properly

--- client/src/pages/Join/Join.js
+++ client/src/pages/Join/Join.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { FormInput, FormTextarea } from "shards-react";
+import React, {useState} from 'react';
+import { FormInput, FormTextarea, Button } from "shards-react";
 import {Container, Row, Col} from "../../components/Grid/Grid"
-export default class Join extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = { value: null };
-  }
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
-  }
+export default function Join() {
 
-  render() {
-    const { value } = this.state;
+    const [userRequest, setUserRequest] = useState({
+        name: "",
+        userName: "",
+        email: "",
+        userBio: ""
+    });
+  
+    const updateUserRequest = (event) => {
+  
+        setUserRequest({
+  
+          ...userRequest,
+          [event.target.name]: event.target.value
+        });
+    };
+
+    const sendUserRequest = (event) => {
+        event.preventDefault();
+        console.log(userRequest)
+    }
+
+
     return (
       <Container>
           <Row>
@@ -23,16 +35,18 @@ export default class Join extends React.Component {
                 <h3>Interested in contributing to Lunchbox Chi? Apply with the fields below and we'll 
                     respond with the appropriate credentials.</h3>
                     <br/>
-                <FormInput placeholder="Name" className="mb-2" />
-                <FormInput placeholder="User Name" className="mb-2" />
-                <FormInput placeholder="email" type="email" className="mb-2" />
-                <p className="mb-2">
-                {(value && `🗣 ${value}`) || "🤔 Waiting for you to say something..."}
-                </p>
-                <FormTextarea onChange={this.handleChange} />
+                <FormInput placeholder="Name" name={'name'} value={userRequest.name} onChange={updateUserRequest} className="mb-2" />
+                <FormInput placeholder="User Name" name={'userName'} value={userRequest.userName} onChange={updateUserRequest} className="mb-2" />
+                <FormInput placeholder="Email" type="email" name={'email'} value={userRequest.email} 
+                onChange={updateUserRequest}className="mb-2" />
+                <br/>
+                <FormTextarea placeholder="Tell us about yourself" size="lg" name={'userBio'}
+                value={userRequest.userBio} onChange={updateUserRequest} />
+                <br/>
+                <Button type="submit" onClick={(event) => sendUserRequest(event)}>Submit</Button>
             </Col>
         </Row>
       </Container>
     );
-  }
+
 }
