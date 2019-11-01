20200eaadab12a150cd827d6ba0c766f87bb7a9a
bee25141 <bee25141@gmail.com>
2019-11-01 16:01:52 -0500

    added pool to mysql connection for db failure testing

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -31,4 +31,40 @@
     console.log("connected as id " + connection.threadId);
   });
 
+  var pool = mysql.createPool(connection);
+
+  exports.connection = {
+      query: function () {
+          var queryArgs = Array.prototype.slice.call(arguments),
+              events = [],
+              eventNameIndex = {};
+
+          pool.getConnection(function (err, conn) {
+              if (err) {
+                  if (eventNameIndex.error) {
+                      eventNameIndex.error();
+                  }
+              }
+              if (conn) { 
+                  var q = conn.query.apply(conn, queryArgs);
+                  q.on('end', function () {
+                      conn.release();
+                  });
+
+                  events.forEach(function (args) {
+                      q.on.apply(q, args);
+                  });
+              }
+          });
+
+          return {
+              on: function (eventName, callback) {
+                  events.push(Array.prototype.slice.call(arguments));
+                  eventNameIndex[eventName] = callback;
+                  return this;
+              }
+          };
+      }
+  };
+
   module.exports = connection;
